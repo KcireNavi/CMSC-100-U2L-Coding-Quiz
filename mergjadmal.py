24b3eba841db3407362077b3769b99d520673491
Ivan Erick Manuel <igmanuel@up.edu.ph>
2026-02-02 18:04:39 +0800

    added mergjadmal.py

---
+++ mergjadmal.py
@@ -0,0 +1,11 @@
+def my_function():
+    userinput = input()
+
+    if "420" in userinput:
+        print("Mergjad!")
+    elif "69" in userinput:
+        print("Mergjad!")
+    else:
+        print("Leim!")
+        
+my_function()
